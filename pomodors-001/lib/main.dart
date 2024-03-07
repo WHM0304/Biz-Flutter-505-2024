5ff6613de2de362142a11f8936486730c5da54f5
WHM0304 <whm0304@naver.com>
2024-03-07 15:57:52 +0900

    pomodors-001

--- pomodors-001/lib/main.dart
+++ pomodors-001/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:timer/dash_page.dart';
 import 'package:timer/home_page.dart';
 import 'package:timer/setting_page.dart';
 
@@ -30,7 +30,24 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  int _pageIndex = 1;
+  int _pageIndex = 0;
+  int wantTimer = 20;
+
+  void onChangeSetting(String value) {
+    if (value.length > 3) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(value)));
+    }
+  }
+
+  /// flutter dart 에서 변수를 선언할때 final, const 키워드가 있으면
+  /// 변수의 type을 명시하지 않아도 된다.
+  /// 단, 이때 반드시 값이 초기화 되어야 한다.
+
+  /// PageController type 의 변수 선언
+  /// HTML tag 에 id 를 적용하는 것처럼 flutter 에서 화면에 표시되는
+  /// component 에 id 를 부착하기 위해 선언하는 변수
+  final _pageController = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
@@ -46,11 +63,43 @@ class _MainPageState extends State<MainPage> {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: PageView(
+          controller: _pageController,
           scrollDirection: Axis.horizontal,
           onPageChanged: (value) => setState(() => _pageIndex = value),
-          children: const [
-            HomePage(),
-            SettingPage(),
+          children: [
+            const HomePage(),
+            const DashPage(),
+            SettingPage(
+              onChange: onChangeSetting,
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _pageIndex,
+          onTap: (value) {
+            _pageIndex = value;
+            _pageController.animateToPage(
+              value,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.ease,
+            );
+
+            /// state를 먼저 변환시키고 마지막에 setState로 상태변환
+            setState(() => {});
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard),
+              label: "dashboard",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "setting",
+            )
           ],
         ),
       ),
--- pomodors-001/lib/setting_page.dart
+++ pomodors-001/lib/setting_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingPage extends StatefulWidget {
-  const SettingPage({super.key});
+  const SettingPage({super.key, required this.onChange});
+  // 한번더 선언해야 밑에 다른 클래스에서 사용가능하다
+  final Function(String) onChange;
 
   @override
   State<SettingPage> createState() => _SettingPageState();
@@ -10,13 +13,54 @@ class SettingPage extends StatefulWidget {
 class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Setting Page",
-          style: TextStyle(fontSize: 80),
-        ),
-      ),
-    );
+    return Scaffold(
+        body: SettingsList(
+      sections: [
+        SettingsSection(
+          title: const Text("타이머설정"),
+          tiles: [
+            SettingsTile(
+              leading: const Icon(Icons.timer_outlined),
+              title: TextField(
+                // 위젯안에 있기때문에 거기서 받아서 처리를해야한다.
+                onChanged: (value) => widget.onChange(value),
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: "일할시간",
+                  contentPadding: EdgeInsets.all(0),
+                  hintText: "타이머 작동시간을 입력해주세요",
+                  hintStyle: TextStyle(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ),
+            SettingsTile(
+              leading: const Icon(Icons.timer_outlined),
+              title: const TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "휴식시간",
+                ),
+              ),
+            ),
+            SettingsTile(
+              leading: const Icon(Icons.timer_outlined),
+              title: const TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "반복횟수",
+                ),
+              ),
+            ),
+            SettingsTile.switchTile(
+              initialValue: true,
+              onToggle: (value) => {},
+              title: const Text("알람"),
+            ),
+          ],
+        )
+      ],
+    ));
   }
 }
