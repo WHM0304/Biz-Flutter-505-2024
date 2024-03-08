46b887bb9a939d747d98aae30cf14da0c3a21f3e
WHM0304 <whm0304@naver.com>
2024-03-08 10:50:26 +0900

    0307

--- pomodors-001/lib/setting_page.dart
+++ pomodors-001/lib/setting_page.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:settings_ui/settings_ui.dart';
 
 class SettingPage extends StatefulWidget {
-  const SettingPage({super.key, required this.onChange});
-  // 한번더 선언해야 밑에 다른 클래스에서 사용가능하다
+  // required this.onChange
+  // main 으로 부터 onChange 라는 props 를 반드시 받도록 선언하기
+  const SettingPage(
+      {super.key, required this.onChange, required this.workTimer});
+  final int workTimer;
+  // State 에서 onChange 를 사용할수 있도록 선언하기
+  // onChange 라는 props 는 함수이다(Function)
+  // 또한 문자열형 매개변수를 통하여 값을 전달할수 있다.
+  // onChange 함수를 사용하는 곳에서 문자열을 전달하면 그 문자열이
+  // main으로 전달된다.
   final Function(String) onChange;
 
   @override
@@ -11,6 +19,14 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingPage> {
+  // id 를 붙이기위한 도구
+  final workTimeController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    workTimeController.text = widget.workTimer.toString();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +38,20 @@ class _SettingPageState extends State<SettingPage> {
             SettingsTile(
               leading: const Icon(Icons.timer_outlined),
               title: TextField(
-                // 위젯안에 있기때문에 거기서 받아서 처리를해야한다.
+                /// TextField 에 id(ref) 부착하기
+                controller: workTimeController,
+
+                /// Stateful 에서 선언한 props 를 사용할때는
+                /// widget.Props 형태로 사용한다
+                /// Stateful 에서 선언한 onChange 함수를
+                /// widget.onChange() 라는 형식으로 사용한다.
+                /// 만약 이 함수에 전달할 값이 필요없다면
+                /// onChanged : widget.onChange 형식으로 사용하면 된다. // 함수만실행
+                /// 하지만 현재 widget.onChange 함수는 문자열형 매개변수를
+                /// 필요로 하도록 선언되어 있다. : final Function(String) onChange;
+                ///
+                /// 필요한 값을 전달 하기 위하여 함수 선언형으로 event 핸들러를
+                /// 사용한다.
                 onChanged: (value) => widget.onChange(value),
                 keyboardType: TextInputType.number,
                 decoration: const InputDecoration(
